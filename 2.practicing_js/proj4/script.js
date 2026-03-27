c16fc52733327a156179deef68c4dd01fd8b2188
flugel047 <128782861+flugel047@users.noreply.github.com>
2026-03-27 10:24:12 +0530

    checking commit

--- 2.practicing_js/proj4/script.js
+++ 2.practicing_js/proj4/script.js
@@ -3,3 +3,4 @@ const modal= document.querySelector('.modal');
 const overlay= document.querySelector('.overlay');
 const btnCloseModal= document.querySelector('.close-modal');
 const btnsOpenModal=document.querySelector('.open-modal');
+console.log('when you was the king of the throne')
