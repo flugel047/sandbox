665861b6795d510de5ab02a7dd90222f1f65110a
flugel047 <128782861+flugel047@users.noreply.github.com>
2026-03-27 11:06:50 +0530

    proj4 done learnt some eventlisteners

--- 2.practicing_js/proj4/script.js
+++ 2.practicing_js/proj4/script.js
@@ -2,5 +2,25 @@
 const modal= document.querySelector('.modal');
 const overlay= document.querySelector('.overlay');
 const btnCloseModal= document.querySelector('.close-modal');
-const btnsOpenModal=document.querySelector('.open-modal');
-console.log('when you was the king of the throne')
+const btnsOpenModal=document.querySelectorAll('.show-modal');
+
+for(let i=0;i<btnsOpenModal.length;i++)
+{
+    btnsOpenModal[i].addEventListener('click',function(){
+        modal.classList.remove('hidden');
+        modal.classList.remove('hidden');
+    })
+}
+const closeModal=function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+btnCloseModal.addEventListener('click',closeModal)
+    
+
+overlay.addEventListener('click',closeModal)
+
+document.addEventListener('keydown',function(e){
+    console.log(e);
+    if(e.key==='Escape') closeModal();
+});
